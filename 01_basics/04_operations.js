507712b6287124ae0a3fd0a0584044de3c3f3bea
Ashutosh <166314867+imashu07@users.noreply.github.com>
2024-08-11 18:28:23 +0000

    complex operations for interview

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,21 @@
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2%2)
+//console.log(2**2)
+
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + str2 
+//console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1" + "2" + 3)
+console.log(1 + 2 + "3")
+console.log(1 + "2" + 3)
