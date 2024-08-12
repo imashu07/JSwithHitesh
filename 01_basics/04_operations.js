aeae3634d2fc059c0b8f024162047413b31946f1
Ashutosh <ashutoshno001@outlook.com>
2024-08-12 23:33:30 +0530

    Operations and resources

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -1,7 +1,7 @@
 let value = 3
 let negValue = -value
-
 //console.log(negValue)
+
 //console.log(2+2)
 //console.log(2-2)
 //console.log(2*2)
@@ -11,7 +11,7 @@ let negValue = -value
 
 let str1 = "hello"
 let str2 = "world"
-let str3 = str1 + str2 
+let str3 = str1 + str2
 //console.log(str3)
 
 console.log("1" + 2)
@@ -19,3 +19,10 @@ console.log("1" + "2")
 console.log("1" + "2" + 3)
 console.log(1 + 2 + "3")
 console.log(1 + "2" + 3)
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+// Refer - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+// Also refer - https://tc39.es/ecma262/multipage/abstract-operations.html
